54996e0698530eb75dd0d380cfe786919292bfe4
Cailly florian <florian.cailly@gmail.com>
2017-04-04 21:24:42 +0200

    gitignore with manifest

--- src/com/marvin/shell/Command.java
+++ src/com/marvin/shell/Command.java
@@ -2,8 +2,6 @@ package com.marvin.shell;
 
 public class Command {
     
-//    protected 
-
     public Command() {
     }
     
--- src/com/marvin/shell/MethodReference.java
+++ src/com/marvin/shell/MethodReference.java
@@ -55,7 +55,4 @@ public class MethodReference {
         return Arrays.deepEquals(this.arguments, other.arguments);
     }
 
-    
-    
-    
 }
